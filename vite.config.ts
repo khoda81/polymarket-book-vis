57448e227823aea52b5e3a32694c82cda73fc6f9
Mahdi Khodabandeh <42507906+khoda81@users.noreply.github.com>
2026-09-17 00:41:26 +0330

    proxy age backend in dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import path from "path";
 
-
 export default defineConfig({
   resolve: {
     alias: {
@@ -10,5 +9,8 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      "/api": "http://localhost:8787",
+    },
   },
 });
